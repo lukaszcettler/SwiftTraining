e5e16aeab9f87eb99b5627a60f367cbe293a3071
Lukasz Cettler <lukasz.cettler@gmail.com>
2022-07-07 13:53:44 +0200

    Building a detail screen

---
+++ PhotoViewer/PhotoViewer/DetailViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+  
+
+}
