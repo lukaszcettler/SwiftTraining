0b229ac7cc59485b7ed21a1755b297783391ac6b
Lukasz Cettler <lukasz.cettler@gmail.com>
2022-07-11 20:20:01 +0200

    Testing WebKit

---
+++ EasyBrowser/EasyBrowser/ViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string: "https://www.onet.pl")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+    }
+
+
+}
+
