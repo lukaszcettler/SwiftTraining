e8ae3c2395655c18643e9c7836d8ee61c9d07e70
Lukasz Cettler <lukasz.cettler@gmail.com>
2022-07-13 14:52:08 +0200

    Parsing JSON

--- WhitehousePetitions/WhitehousePetitions/ViewController.swift
+++ WhitehousePetitions/WhitehousePetitions/ViewController.swift
@@ -2,11 +2,28 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    var petitions = [String]()
+    var petitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        
+        if let url = URL(string: urlString){
+            if let data = try? Data(contentsOf: url){
+                parse(json: data)
+            }
+        }
 
+    }
+    
+    func parse(json: Data){
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json){
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -15,8 +32,10 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell_id", for: indexPath)
-        cell.textLabel?.text = "Title"
-        cell.detailTextLabel?.text = "Subtitle"
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 12)
+        cell.detailTextLabel?.text = petition.body
         return cell
     }
 
